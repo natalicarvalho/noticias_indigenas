8db1e6643677676b5e421e03888134cda4a797b3
Natali Carvallho <66014118+natalicarvalho@users.noreply.github.com>
2023-04-03 13:29:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ def dedoduro2():
               
 @app.route("/jornais")
 def qualquer_coisa():
-    resp = requests.get('https://uol.com.br)
+    resp = requests.get('https://uol.com.br')
     mensagem = {"chat_id": 42, "text": resp.text()}
     requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=mensagem)
     return "Aqui está os termos"
