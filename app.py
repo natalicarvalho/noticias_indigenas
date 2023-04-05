52c1c80020dddca6bf9b19c7454237d7c4364eed
Natali Carvallho <66014118+natalicarvalho@users.noreply.github.com>
2023-04-05 18:10:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -181,7 +181,6 @@ def dedoduro2():
  
 @app.route("/jornais")
 def jornais():
-    resp = requests.get(envia_mensagem)
     mensagem = {"chat_id": TELEGRAM_ADMIN_ID, "text": resp.text()}
     requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=mensagem)
     return "Aqui estão os termos"
