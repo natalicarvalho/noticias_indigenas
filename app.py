30a410f958788674f29881c3a8fafaf4e2002b87
Natali Carvallho <66014118+natalicarvalho@users.noreply.github.com>
2023-04-08 10:32:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,146 +11,8 @@ TELEGRAM_ADMIN_ID = os.environ["TELEGRAM_ADMIN_ID"]
 
 app = Flask(__name__)
 
-
-lista_url = [
-    'https://feeds.folha.uol.com.br/ambiente/rss091.xml',
-    'https://extra.globo.com/rss.xml',
-    'https://www.gazetadopovo.com.br/rss/',
-    'https://g1.globo.com/rss/g1/',
-    'https://www.uol.com.br/vueland/api/?loadComponent=XmlFeedRss'
-]
-
-
-def items(url):
-    resp = requests.get(url)
-    try:
-        data = xmltodict.parse(resp.content)
-    except Exception as error:
-        print(f"Erro baixando dados de {url}: {error}")
-        return []
-
-    return data['rss']['channel']['item']
-
-
-def pega_link(url_jornal):
-    resultado = items(url_jornal)
-    lista = []
-    for item in resultado:
-        url = item.get('link')
-        desc = item.get('description')
-        tit = item.get('title')
-        dat = item.get('pubDate')
-        resultado_formatado = {"url": url,
-                              "descricao": desc,
-                              "titulo": tit,
-                              "data": dat
-                              }
-        lista.append(resultado_formatado)
-
-    termos = ['indígena', 'Indígena', 'Yanomami', 'índio', 'demarcação']
-    links_que_tem_termos = []
-
-    for item_formatado in lista:
-        for termo in termos:
-            if termo in item_formatado["descricao"]:
-                print(item_formatado["titulo"])
-                links_que_tem_termos.append([termo, item_formatado["url"]])
-                # break
-
-    return links_que_tem_termos
-
-
-def raspa_dados():
-    links_salvos = []
-    for link in lista_url:
-        print(link)
-
-        resultados_link = pega_link(link)
-        if resultados_link is not None:
-            for x in resultados_link:
-                print(x)
-            links_salvos.extend(resultados_link)
-            
-    dados_link = []
-    for link in links_salvos():
-        for item in dado:
-            print(item)
-            dados_link.append(item)
-    return dados_link
-          
-   
-def conta_reportagem(dados, texto_resposta):
-    header = "\nQuantidade de reportagens por tema, selecione o número para receber as urls:\n"
-    texto_resposta += header
-    numero_contador = 0
-    for termo, quantidade in dados.value_counts().iteritems():
-      numero_contador = numero_contador + 1
-      print(termo, quantidade)
-      texto_resposta += f"{str(numero_contador)} - {termo}: {quantidade}\n"
-    
-    return texto_resposta
-
-def criar_resposta(message, dados):
-    texto_resposta = " "
-    if message == "Oi":
-        texto_resposta = "Olá você iniciou o Bot de Notícias."
-        texto_resposta = conta_reportagem(dados['termo'],texto_resposta) 
-    else:
-        try:
-            if int(message) < len(dados['termo']):
-                envia_links(dados, int(message))
-                
-        except ValueError:
-            texto_resposta = "Não entendi a mensagem."
-    
-    return texto_resposta
-    
-def envia_links(dados, opcao):
-    opcao = opcao - 1
-    termo = dados['termo'].value_counts().keys()[opcao]
-    links_dos_termos = dados[dados['termo']== termo]['link']
-    texto = ''
-    for link in links_dos_termos:
-        texto = texto + f"🔗 {link}\n\n"
-  
-    return texto
-
-def print_para_o_render(update):
-    first_name = update["message"]["from"]["first_name"]
-    message = update["message"]["text"]
-    chat_id = update["message"]["chat"]["id"]
-    datahora = str(datetime.datetime.fromtimestamp(update["message"]["date"]))
-    
-    if "username" in update["message"]["from"]:
-        username = update["message"]["from"]["username"]
-    else:
-        username = "[não definido]"
-        
-    print(
-        f"[{datahora}] Nova mensagem de {first_name} @{username} ({chat_id}): {message}"
-    )
-   
-    
-def envia_mensagem(update): 
-    if "text" not in update["message"]:
-        return  # Essa mensagem não é um texto!
-        
-    print_para_o_render(update)
-    
-    message = update["message"]["text"]
-    chat_id = update["message"]["chat"]["id"]
-    
-    dados = raspa_dados()
-    text = criar_resposta(message, dados)
-    nova_mensagem = {"chat_id": chat_id, "text": text}
-    requests.post(
-        f"https://api.telegram.org/bot{TELEGRAM_API_KEY}/sendMessage",
-        data=nova_mensagem,
-    )
-
 menu = """
 <a href="/">Página inicial</a> |
-<a href="/promocoes">PROMOÇÕES</a> |
 <a href="/sobre">Sobre</a> |
 <a href="/contato">Contato</a>
 <br>
@@ -166,18 +28,53 @@ def sobre():
 
 @app.route("/contato")
 def contato():
-  return menu + "Aqui vai o conteúdo da página Contato"
-
-
-@app.route("/dedoduro")
-def dedoduro():
-  mensagem = {"chat_id": TELEGRAM_ADMIN_ID, "text": "Alguém acessou a página dedo duro!"}
-  resposta = requests.post(f"https://api.telegram.org/bot{TELEGRAM_API_KEY}/sendMessage", data=mensagem)
-  return f"Mensagem enviada. Resposta ({resposta.status_code}): {resposta.text}"
-
-         
+  return menu + "Aqui vai o conteúdo da página Contato"        
  
-@app.route("/jornais", methods=["POST"])
-def jornais():
-    envia_mensagem(request.json)
-    return "ok"
+@app.route("/telegrambot", methods=["POST"])
+def telegrambot():
+    message = update["message"]["text"]
+    chat_id = update["message"]["chat"]["id"]
+    
+    dados = raspa_dados()
+    text = criar_resposta(message, dados)
+    nova_mensagem = {"chat_id": chat_id, "text": text}
+    requests.post(
+        f"https://api.telegram.org/bot{TELEGRAM_API_KEY}/sendMessage",
+        data=nova_mensagem,
+    )
+    
+    def conta_reportagem(dados, texto_resposta):
+        header = "\nQuantidade de reportagens por tema, selecione o número para receber as urls:\n"
+        texto_resposta += header
+        numero_contador = 0
+        for termo, quantidade in dados.value_counts().iteritems():
+            numero_contador = numero_contador + 1
+            print(termo, quantidade)
+            texto_resposta += f"{str(numero_contador)} - {termo}: {quantidade}\n"
+    
+        return texto_resposta
+    
+    def criar_resposta(message, dados):
+        texto_resposta = " "
+        if message == "Oi":
+            texto_resposta = "Olá você iniciou o Bot de Notícias."
+            texto_resposta = conta_reportagem(dados['termo'],texto_resposta) 
+        else:
+            try:
+                if int(message) < len(dados['termo']):
+                    envia_links(dados, int(message))
+                
+            except ValueError:
+                texto_resposta = "Não entendi a mensagem."
+    
+        return texto_resposta
+    
+    def envia_links(dados, opcao):
+        opcao = opcao - 1
+        termo = dados['termo'].value_counts().keys()[opcao]
+        links_dos_termos = dados[dados['termo']== termo]['link']
+        texto = ''
+        for link in links_dos_termos:
+            texto = texto + f"🔗 {link}\n\n"
+  
+        return texto
