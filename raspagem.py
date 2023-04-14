d13ffc243463c265e9d6c8a4419f974c48ae9004
Natali Carvallho <66014118+natalicarvalho@users.noreply.github.com>
2023-04-14 14:48:30 -0300

    Update raspagem.py

--- raspagem.py
+++ raspagem.py
@@ -62,10 +62,9 @@ def raspa_dados():
 
         resultados_link = pega_link(link)
         if resultados_link is not None:
-            if isinstance(resultados_link, (list, tuple)):
-                for x in resultados_link:
-                    print(x)
-                links_salvos.extend(resultados_link)
+            for x in resultados_link:
+                print(x)
+            links_salvos.extend(resultados_link)
             
     dados_link = pd.DataFrame(links_salvos, columns=["termo", "link"])
         
